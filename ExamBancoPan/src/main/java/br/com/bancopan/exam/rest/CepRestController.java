8174536ea0c9bd4e698a254307af4d7ca4452822
JeanDev <Jean@192.168.0.10>
2020-04-10 12:43:01 -0300

    Commit - Commit de funcionalidade

--- ExamBancoPan/src/main/java/br/com/bancopan/exam/rest/CepRestController.java
+++ ExamBancoPan/src/main/java/br/com/bancopan/exam/rest/CepRestController.java
@@ -1,6 +1,8 @@
 package br.com.bancopan.exam.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,13 @@ public class CepRestController {
 	private EnderecoService enderecoService;
 
 	@GetMapping("/{codigoCep}")
-	public Cep consultarCep(@RequestParam String codigoCep) {
-		return enderecoService.consultarCep(codigoCep);
+	public ResponseEntity<Cep> consultarCep(@RequestParam String codigoCep) {
+		Cep cep = enderecoService.consultarCep(codigoCep);
+		if (cep != null) {
+			return new ResponseEntity<>(cep, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 }
