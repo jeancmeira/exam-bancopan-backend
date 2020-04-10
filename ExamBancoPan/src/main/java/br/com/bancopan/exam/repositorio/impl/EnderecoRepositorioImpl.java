8be332ca97c714a9fbf68ad9841b6f58e9ac80df
JeanDev <Jean@192.168.0.10>
2020-04-10 12:58:08 -0300

    Commit - Commit de funcionalidade

--- ExamBancoPan/src/main/java/br/com/bancopan/exam/repositorio/impl/EnderecoRepositorioImpl.java
+++ ExamBancoPan/src/main/java/br/com/bancopan/exam/repositorio/impl/EnderecoRepositorioImpl.java
@@ -1,28 +1,61 @@
 package br.com.bancopan.exam.repositorio.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 import br.com.bancopan.exam.domain.Cep;
 import br.com.bancopan.exam.domain.Estado;
 import br.com.bancopan.exam.domain.Municipio;
 import br.com.bancopan.exam.repositorio.def.EnderecoRepositorio;
 
+@Component
 public class EnderecoRepositorioImpl implements EnderecoRepositorio {
 
 	@Override
 	public Cep consultarCep(String codigoCep) {
-		return null;
+		
+		Estado estado = getEstado();
+		
+		Municipio municipio = getMunicipio(estado);
+		
+		Cep cep = new Cep();
+		cep.setCodigo("04349000");
+		cep.setLogradouro("RUA DAS GRUMIXAMAS");
+		cep.setMunicipio(municipio);
+		
+		return cep;
+	}
+
+	private Municipio getMunicipio(Estado estado) {
+		Municipio municipio = new Municipio();
+		municipio.setEstado(estado);
+		municipio.setNome("SAO PAULO");
+		return municipio;
+	}
+
+	private Estado getEstado() {
+		Estado estado = new Estado();
+		estado.setId(1L);
+		estado.setSigla("SP");
+		estado.setNome("SAO PAULO");
+		return estado;
 	}
 
 	@Override
 	public List<Estado> listarEstados() {
-		return null;
+		List<Estado> estados = new ArrayList<>();
+		estados.add(getEstado());
+		return estados;
 	}
 
 	@Override
 	public List<Municipio> consultarMunicipios(Long idEstado) {
-		// TODO Auto-generated method stub
-		return null;
+		Estado estado = getEstado();
+		List<Municipio> municipios = new ArrayList<>();
+		municipios.add(getMunicipio(estado));
+		return municipios;
 	}
 
 }
--- ExamBancoPan/src/main/java/br/com/bancopan/exam/service/impl/EnderecoServiceImpl.java
+++ ExamBancoPan/src/main/java/br/com/bancopan/exam/service/impl/EnderecoServiceImpl.java
@@ -1,61 +1,35 @@
 package br.com.bancopan.exam.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.bancopan.exam.domain.Cep;
 import br.com.bancopan.exam.domain.Estado;
 import br.com.bancopan.exam.domain.Municipio;
+import br.com.bancopan.exam.repositorio.def.EnderecoRepositorio;
 import br.com.bancopan.exam.service.def.EnderecoService;
 
 @Service
 public class EnderecoServiceImpl implements EnderecoService {
 
+	@Autowired
+	private EnderecoRepositorio enderecoRepositorio;
+	
 	@Override
 	public Cep consultarCep(String codigoCep) {
-		
-		Estado estado = getEstado();
-		
-		Municipio municipio = getMunicipio(estado);
-		
-		Cep cep = new Cep();
-		cep.setCodigo("04349000");
-		cep.setLogradouro("RUA DAS GRUMIXAMAS");
-		cep.setMunicipio(municipio);
-		
-		return cep;
-	}
-
-	private Municipio getMunicipio(Estado estado) {
-		Municipio municipio = new Municipio();
-		municipio.setEstado(estado);
-		municipio.setNome("SAO PAULO");
-		return municipio;
-	}
-
-	private Estado getEstado() {
-		Estado estado = new Estado();
-		estado.setId(1L);
-		estado.setSigla("SP");
-		estado.setNome("SAO PAULO");
-		return estado;
+		return enderecoRepositorio.consultarCep(codigoCep);
 	}
 
 	@Override
 	public List<Estado> listarEstados() {
-		List<Estado> estados = new ArrayList<>();
-		estados.add(getEstado());
-		return estados;
+		return enderecoRepositorio.listarEstados();
 	}
 
 	@Override
 	public List<Municipio> consultarMunicipios(Long idEstado) {
-		Estado estado = getEstado();
-		List<Municipio> municipios = new ArrayList<>();
-		municipios.add(getMunicipio(estado));
-		return municipios;
+		return enderecoRepositorio.consultarMunicipios(idEstado);
 	}
 
 }
