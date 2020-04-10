521e890a25cad900b12aa7d45030991e4972c0e8
JeanDev <Jean@192.168.0.10>
2020-04-10 11:46:46 -0300

    Commit - Commit de funcionalidade

--- ExamBancoPan/src/main/java/br/com/bancopan/exam/domain/Cliente.java
+++ ExamBancoPan/src/main/java/br/com/bancopan/exam/domain/Cliente.java
@@ -1,4 +1,4 @@
-package br.com.bancopan.exam.model;
+package br.com.bancopan.exam.domain;
 
 public class Cliente {
 
--- ExamBancoPan/src/main/java/br/com/bancopan/exam/rest/ClienteRestController.java
+++ ExamBancoPan/src/main/java/br/com/bancopan/exam/rest/ClienteRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.bancopan.exam.model.Cliente;
+import br.com.bancopan.exam.domain.Cliente;
 
 @RestController
 @RequestMapping("/cliente")
